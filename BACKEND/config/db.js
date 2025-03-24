e5b663d9652df48008fb6b1fed6ba8daab893b62
sanjay <sanjayraveendran.48@gmail.com>
2025-03-24 14:31:03 +0530

    Updated by me

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,11 +1,11 @@
-import mongoose  from 'mongoose';
+import mongoose from 'mongoose'; 
 
-export const connectDB=async ()=>{
-    try{
-        const conn=await mongoose.connect("process.env.MONGO_URI");
-        console.log(`MongoDB Connected:${conn.connection.host}`);
-    }  catch(error){
-        console.log(`ERROR: ${error.message}`);
-        process.exit(1);   
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`ERROR: ${error.message}`);
+        process.exit(1); 
     }
-}
+};
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,14 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
+
 dotenv.config(); 
 
 const app=express();
 
-app.get("/",(req,res)=>{
-.console.log('Server is running on port 5000');
-console.log('Environment:', process.env.NODE_ENV);
-console.log('Database connection:', process.env.MONGO_URI);    
+
+app.get("",(req,res)=>{
 })
 
 console.log(process.env.MONGO_URI);
